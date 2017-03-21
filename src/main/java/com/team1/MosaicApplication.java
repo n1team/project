159cc76e5bb2f86f06d1c60a53d6e9bded2e4a51
n1team <team1@mt2015.com>
2017-03-21 14:27:08 +0900

    e

---
+++ src/main/java/com/team1/MosaicApplication.java
@@ -0,0 +1,12 @@
+package com.team1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosaicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosaicApplication.class, args);
+	}
+}
